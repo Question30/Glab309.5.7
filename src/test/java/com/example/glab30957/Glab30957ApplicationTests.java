da4991b3f603f94e5a8302c970beaf19c0970632
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-08-14 16:51:12 -0400

    Glab done

---
+++ src/test/java/com/example/glab30957/Glab30957ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glab30957;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Glab30957ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
